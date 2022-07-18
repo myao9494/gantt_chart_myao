01dc150e063d676bc635223bbe066e54fc7d8a15
myao9494 <mineouka@outlook.jp>
2022-07-19 06:02:05 +0900

    python系途中

--- 000_TOP.py
+++ 000_TOP.py
@@ -27,13 +27,84 @@
 
 import DB_python ; obj = DB_python.db_con()
 
-taisho_start = "2022-6-27"
+taisho_start = "2022-7-4"
+# taisho_start = "2022-7-11"
 nobasu_day = 7
 obj.move_task(taisho_start,nobasu_day)
 
-# # 予定を作成する
+# ## 予定を作成する
+
+# 予定を確認
+
+import DB_python ; self = DB_python.db_con()
+
+df = self.create_db()
+
+df_sc = df[["id","text","task_schedule"]].copy().dropna()
+df_sc = df_sc[df_sc["task_schedule"]!=""]
+
+df_sc
+
+qg = DB_python.qgid_local(df_sc)
+
+qg
+
+# 予定を編集
+
+import pandas  as pd
+
+df_sc_edit = qg.get_changed_df()
+df_sc.compare(df_sc_edit)
 
 
+
+self.to_db(df_sc_edit.loc[8:9],"task_schedule")
+
+# 新規作成
+
+id_num = 15
+
+id_num in list(df_sc_edit["id"])
+
+if id_num  in list(df_sc["id"]):
+    out = df_sc.loc[id_num,"task_schedule"] + "___" + yotei_str
+    df_out = pd.DataFrame([[id_num,out]],columns=["id","task_schedule"])
+    print(out)
+else:
+    out = yotei_str + "___" 
+    df_out = pd.DataFrame([[id_num,out]],columns=["id","task_schedule"])
+
+df_out
+
+rec = df_out.iloc[0:1][["id", "task_schedule"]].to_dict("records")[0]
+
+rec
+
+
+
+
+
+ values(id=str(rec["id"]), data=rec["task_schedule"])
+
+
+
+self.to_db(df_out,"task_schedule")
+
+import pandas as pd
+
+
+
+
+
+id_num = 107
+yotei = "上西内科"
+date = "2022-7-22"
+length = 1
+biko = ""
+
+yotei_str = f"{date},{yotei},{str(length)},{biko}"
+
+self.to_db()
 
 
 
@@ -49,7 +120,16 @@ out = f'[{str(id_num)},"{yotei}",new Date({date.year},{date.month-1},{date.day})
 
 
 
+# +
+import qgrid
+import pandas as pd
+url = 'https://github.com/chris1610/pbpython/blob/master/data/2018_Sales_Total_v2.xlsx?raw=True'
+df = pd.read_excel(url)
 
+widget = qgrid.show_grid(df)
+# -
+
+widget
 
 # ## 過去
 
--- DB_python.py
+++ DB_python.py
@@ -12,62 +12,99 @@
 #     name: python3
 # ---
 
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String,DATETIME
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DATETIME
 from sqlalchemy.dialects.mysql import insert
 import pandas as pd
 # import utility
+import qgrid
+
+
+def qgid_local(df):
+    col_options = {
+        'width': 10,
+    }
+    col_defs = {
+        'text': {
+            'width': 100,
+        },
+        'task_schedule': {
+            'width': 400,
+        }
+    }
+
+    qgrid_widget = qgrid.show_grid(df,
+                                   column_options=col_options,
+                                   column_definitions=col_defs,
+                                   show_toolbar=True)
+    return qgrid_widget
+
 
 class db_con(object):
     '''
     pythonからmysqlのDBへアクセスする
     '''
+
     def __init__(self):
-        pd.set_option("display.max_colwidth",10000)
+        pd.set_option("display.max_colwidth", 10000)
         pd.set_option('display.max_rows', 500)
-        self.user="root"
-        self.password=""
-        self.host="localhost"
-        self.port=3306
-        self.database_1 ="gantt_howto_node"
+        self.user = "root"
+        self.password = ""
+        self.host = "localhost"
+        self.port = 3306
+        self.database_1 = "gantt_howto_node"
         self.url_1 = f'mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database_1}?charset=utf8'
-        self.engine_1 = create_engine(self.url_1,echo=False)
+        self.engine_1 = create_engine(self.url_1, echo=False)
 
     def create_db(self):
         query = "select * from gantt_tasks"
-        self.df = pd.read_sql(query,con=self.engine_1)
+        self.df = pd.read_sql(query, con=self.engine_1)
         return self.df
 
-    def move_task(self,taisho_start,nobasu_day):
-#         if not hasattr(self,"df"):
+    def move_task(self, taisho_start, nobasu_day):
+        #         if not hasattr(self,"df"):
         self.create_db()
         df_taisho = self.df.copy()
-        df_taisho = df_taisho[df_taisho["start_date"]==taisho_start]
-        df_taisho["start_date"] = df_taisho["start_date"] + pd.Timedelta(days = nobasu_day)
-        
+        df_taisho = df_taisho[df_taisho["start_date"] == taisho_start]
+        df_taisho["start_date"] = df_taisho["start_date"] + \
+            pd.Timedelta(days=nobasu_day)
+        out = self.to_db(df_taisho, "start_date")
+        print(out)
+
+    def to_db(self, df_taisho, retu_mei):
         if len(df_taisho) != 0:
             metadata_1 = MetaData()
             metadata_1.bind = self.engine_1
             menus = Table(
-              'gantt_tasks', metadata_1,
-              Column('id', Integer, primary_key=True),
-              Column('start_date', DATETIME),
+                'gantt_tasks', metadata_1,
+                Column('id', Integer, primary_key=True),
+                Column(retu_mei, DATETIME),
             )
 
             conn = self.engine_1.connect()
 
             for index in range(len(df_taisho)):
-                rec = df_taisho.iloc[index:index+1][["id","start_date"]].to_dict("records")
+                rec = df_taisho.iloc[index:index +
+                                     1][["id", retu_mei]].to_dict("records")
                 insert_stmt = insert(menus).values(rec)
-                on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update(
-                    start_date=insert_stmt.inserted.start_date
-                )
-                conn.execute(on_duplicate_key_stmt)
+                # on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update(
+                #     start_date=insert_stmt.inserted.start_date
+                # )
+                command = f"conn.execute(insert_stmt.on_duplicate_key_update({retu_mei}=insert_stmt.inserted.{retu_mei}))"
+                exec(command)
 #                 print(index,rec)
             conn.close()
+            out = f"{len(df_taisho)}件処理しました"
         else:
-            print("０件でした")
+            out = "０件でした"
+        return out
+
 
 # ## test
+
+# df_taisho = pd.DataFrame([{'id': 107, 'task_schedule': '2022-7-22,上西内科,1,___'}
+#                           ])
+
+# df_taisho
 
 # +
 # self = db_con()
@@ -79,3 +116,27 @@ class db_con(object):
 # self.move_task(taisho_start,nobasu_day)
 
 
+# -
+
+# retu_mei = "task_schedule"
+
+# metadata_1 = MetaData()
+# metadata_1.bind = self.engine_1
+# menus = Table(
+#     'gantt_tasks', metadata_1,
+#     Column('id', Integer, primary_key=True),
+#     Column(retu_mei, DATETIME),
+# )
+
+# conn = self.engine_1.connect()
+
+
+# rec = df_taisho.iloc[index:index+1][["id", retu_mei]].to_dict("records")
+# insert_stmt = insert(menus).values(rec)
+
+
+# command = f"on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update({retu_mei}=insert_stmt.inserted.{retu_mei})"
+
+# exec(command)
+
+# conn.execute(on_duplicate_key_stmt)
